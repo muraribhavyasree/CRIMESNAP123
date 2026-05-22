85764a7954630a6b04d956f2cd79439a48355d78
Murari Bhavya Sree <muraribhavyasree19@gmail.com>
2026-05-22 12:34:11 +0530

    crs

--- frontend/front/src/socket.js
+++ frontend/front/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://crime-snap-main-1.onrender.com", {
+const socket = io("https://crimesnap123-1.onrender.com", {
   transports: ["websocket"],
 });
 
